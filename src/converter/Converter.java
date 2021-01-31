0c4d018c99d97bc137ca6ca1395512d5d101b02b
Herb01 <herb1@ziggo.nl>
2021-01-31 22:55:30 +0100

    Initialise project

---
+++ src/converter/Converter.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package converter;
+
+/**
+ *
+ * @author Herbert
+ */
+public class Converter {
+    private double fahrenheit;
+    private double celcius;
+
+    /**
+     * @return the fahrenheit
+     */
+    public double getFahrenheit() {
+        return fahrenheit;
+    }
+
+    /**
+     * @param fahrenheit the fahrenheit to set
+     */
+    public void setFahrenheit(double fahrenheit) {
+        this.fahrenheit = fahrenheit;
+    }
+
+    /**
+     * @return the celcius
+     */
+    public double getCelcius() {
+        return celcius;
+    }
+
+    /**
+     * @param celcius the celcius to set
+     */
+    public void setCelcius(double celcius) {
+        this.celcius = celcius;
+    }
+  
+}
